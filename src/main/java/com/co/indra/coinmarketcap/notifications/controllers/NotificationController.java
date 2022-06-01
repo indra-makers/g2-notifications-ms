f38835ea5f9c74fa43735a8847dc7f3508a331b8
Daniel Alarcón Tabares <daniel.alarcon@utp.edu.co>
2022-06-01 13:23:29 -0500

    Daniel: Notificaciones+Swagger+readyForHeroku

--- src/main/java/com/co/indra/coinmarketcap/notifications/controllers/NotificationController.java
+++ src/main/java/com/co/indra/coinmarketcap/notifications/controllers/NotificationController.java
@@ -1,13 +1,30 @@
 package com.co.indra.coinmarketcap.notifications.controllers;
 
+import com.co.indra.coinmarketcap.notifications.config.Routes;
+import com.co.indra.coinmarketcap.notifications.model.entities.Notification;
 import com.co.indra.coinmarketcap.notifications.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(Routes.NOTIFICATION_SEND_PATH)
 public class NotificationController {
 
     @Autowired
     private NotificationService notificationService;
+
+    @PostMapping
+    public void registerNotification(@RequestBody Notification notification) {
+        notificationService.registerNotification(notification);
+    }
+
+    @GetMapping(Routes.NOTIFICATION_VIEWLOG_BY_EMAIL)
+    public List<Notification> getPortafoliosUserName(
+            @PathVariable("mailuser") String mailuser) {
+        return notificationService.getNotification(mailuser);
+    }
+
 
 }
--- src/main/java/com/co/indra/coinmarketcap/notifications/repositories/NotificationsRepository.java
+++ src/main/java/com/co/indra/coinmarketcap/notifications/repositories/NotificationsRepository.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 
 class NotificationsRowMapper implements RowMapper<Notification> {
         @Override
         public Notification mapRow(ResultSet rs, int rowNum) throws SQLException {
             Notification notification = new Notification();
-            notification.set_idNotification(rs.getString("idnotification"));
+            notification.set_idNotification(rs.getString("id_notification"));
             notification.settypeOf(rs.getString("typeof"));
             notification.setMessage(rs.getString("message"));
             notification.setSent_at(rs.getTimestamp("sent_at"));
@@ -25,7 +26,19 @@ class NotificationsRowMapper implements RowMapper<Notification> {
 }
 
 @Repository
-public class NotificationsRepository{
+public class NotificationsRepository {
     @Autowired
     private JdbcTemplate template;
+
+    public void sendNotification(Notification notification) {
+        template.update("INSERT INTO tbl_notifications( typeof, message, mailuser, subject, phonenumber) values(?,?,?,?,?)",
+                notification.gettypeOf(), notification.getMessage(), notification.getMailUser(),
+                notification.getSubject(), notification.getphoneNumber());
+    }
+
+    public List<Notification> logNotificationsByEmail(String mailuser) {
+        return template.query(
+                "SELECT id_notification, typeof, message, sent_at, mailuser, subject, phonenumber FROM tbl_notifications WHERE mailuser=?",
+                new NotificationsRowMapper(), mailuser);
+    }
 }
