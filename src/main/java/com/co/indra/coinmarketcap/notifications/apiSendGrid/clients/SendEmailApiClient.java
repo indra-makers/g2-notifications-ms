99979638b9aea9db67b965ff9d3607440a10d3c4
abuitragog <105449505+abuitragogo@users.noreply.github.com>
2022-06-09 07:42:36 -0500

    Req. sendgrid Email

--- src/main/java/com/co/indra/coinmarketcap/notifications/apiSendGrid/clients/SendEmailApiClient.java
+++ src/main/java/com/co/indra/coinmarketcap/notifications/apiSendGrid/clients/SendEmailApiClient.java
@@ -7,6 +7,7 @@ import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.sendgrid.helpers.mail.objects.Email;
 
@@ -15,13 +16,19 @@ import java.io.IOException;
 @Component
 public class SendEmailApiClient {
 
+    @Value("${api.sendgrid.apikey}")
+    private String sendGridApiKey;
+
+    @Value("${api.sendgrid.from}")
+    private String sendGridFrom;
+
     public void sendEmail(String subject, String body, String to){
-        Email from = new Email("desarrolloenequipoeam@gmail.com");
+        Email from = new Email(sendGridFrom);
         Email para = new Email(to.toString());
         Content content = new Content("text/plain",body);
         Mail mail = new Mail(from, subject, para, content);
 
-        SendGrid sg = new SendGrid(System.getenv("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"));
+        SendGrid sg = new SendGrid(sendGridApiKey);
         Request request = new Request();
         try{
             request.setMethod(Method.POST);
