5254e1d2aea1906d0a6a30a0a1dc0ee94c0f8edb
deisygarcia188 <carolinagarciam10@gmail.com>
2022-06-12 16:49:20 -0500

    Deisy: Mesajeria en user

--- src/main/java/com/co/indra/coinmarketcap/notifications/services/UserService.java
+++ src/main/java/com/co/indra/coinmarketcap/notifications/services/UserService.java
@@ -53,4 +53,8 @@ public class UserService {
     public List<Users> getAllUsers(){
         return usersRepository.listAllUsers();
     }
+
+    public void saveUser(String userId, String mailUser, String phoneNumber) {
+        usersRepository.addUser(new Users(userId, mailUser, phoneNumber));
+    }
 }
